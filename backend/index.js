d1236ed6a07a50e078182557d0393bd91afe8361
Mauro Patricio <mpatricio@cedsif.mf.gov.mz>
2023-05-04 06:32:52 +0200

    Actualizacao

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(rootDir+'/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 
