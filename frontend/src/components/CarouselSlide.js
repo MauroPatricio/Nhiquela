ba695b0d4cf088cf67170fa9a6d60e31f46d3b9a
Mauro Patricio <mpatricio@cedsif.mf.gov.mz>
2023-08-06 07:46:06 +0200

    Actualizacao

--- frontend/src/components/CarouselSlide.js
+++ frontend/src/components/CarouselSlide.js
@@ -52,7 +52,7 @@ export default function CarouselSlide() {
     <>
 <br/>
      {popularItems && <h3>Produtos em Destaque</h3>} 
-    <Carousel showArrows infiniteLoop={true} autoPlay showThumbs={false}  showIndicators={false} className='carousel-custom'>
+    <Carousel showArrows infiniteLoop={true}  autoPlay showThumbs={false}  showIndicators={false} className='carousel-custom'>
       {popularItems && popularItems.map((p) => (
        <Link className="link" to={`/product/${p.slug}`} key={p._id}>
         <div key={p._id}>
