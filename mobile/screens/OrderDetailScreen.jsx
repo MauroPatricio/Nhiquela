7e8e5c3a9007a44873133d1d3a64cb785743e840
Mauro Patricio <mpatricio@cedsif.mf.gov.mz>
2024-09-09 12:29:16 +0200

    Actualizaco

--- mobile/screens/OrderDetailScreen.jsx
+++ mobile/screens/OrderDetailScreen.jsx
@@ -1,39 +1,42 @@
+import {useRoute} from '@react-navigation/native'
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet,  Image, ScrollView } from 'react-native';
 
-const OrderDetailsScreen = (item) => {
-  // const { item } = route.params;
+const OrderDetailsScreen = () => {
+  const {params: {
+    item
+          } }= useRoute();
 
-  const order = item.item
+  const order = item
 
-  return (
+      return (
+     
     <ScrollView style={styles.container}>
-      {/* <Text style={styles.header}>Order Details</Text>
+      <Text style={styles.heading}>Detalhes do pedido</Text>
       
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>Customer Info</Text>
-        <Text style={styles.label}>Name: <Text style={styles.value}>{item.deliveryAddress.fullName}</Text></Text>
-        <Text style={styles.label}>Phone: <Text style={styles.value}>{item.deliveryAddress.phoneNumber}</Text></Text>
-        <Text style={styles.label}>City: <Text style={styles.value}>{item.deliveryAddress.city}</Text></Text>
-        <Text style={styles.label}>Address: <Text style={styles.value}>{item.deliveryAddress.address}</Text></Text>
-        <Text style={styles.label}>Reference: <Text style={styles.value}>{item.deliveryAddress.referenceAddress}</Text></Text>
-      </View>
+      <Text style={styles.label}>Codigo do pedido: <Text style={styles.value}>{order.code}</Text></Text>
+      <Text style={styles.label}>Forma de pagamento: <Text style={styles.value}>{order.paymentMethod}</Text></Text>
+      <Text style={styles.label}>Preco total pago: <Text style={styles.price}>{order.totalPrice} MT</Text></Text>
+      <Text style={styles.label}>Estado: <Text style={styles.value}>{order.status}</Text></Text>
+      {/* <Text style={styles.label}>Items Price: <Text style={styles.value}>${order.itemsPrice}</Text></Text>
+      <Text style={styles.label}>Site Tax: <Text style={styles.value}>${order.siteTax}</Text></Text>
+      <Text style={styles.label}>IVA Tax: <Text style={styles.value}>${order.ivaTax}</Text></Text>
+      <Text style={styles.label}>Address Price: <Text style={styles.value}>${order.addressPrice}</Text></Text> */}
 
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>Order Info</Text>
-        <Text style={styles.label}>Order Code: <Text style={styles.value}>{item.code}</Text></Text>
-        <Text style={styles.label}>Payment Method: <Text style={styles.value}>{item.paymentMethod}</Text></Text>
-        <Text style={styles.label}>Status: <Text style={styles.value}>{item.status}</Text></Text>
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.sectionHeader}>Pricing</Text>
-        <Text style={styles.label}>Items Price: <Text style={styles.value}>{item.itemsPrice}</Text></Text>
-        <Text style={styles.label}>IVA Tax: <Text style={styles.value}>{item.ivaTax}</Text></Text>
-        <Text style={styles.label}>Delivery Price: <Text style={styles.value}>{item.addressPrice}</Text></Text>
-        <Text style={styles.label}>Site Tax: <Text style={styles.value}>{item.siteTax}</Text></Text>
-        <Text style={styles.label}>Total Price: <Text style={styles.value}>{item.totalPrice}</Text></Text>
-      </View> */}
+      <Text style={styles.subheading}>Produtos:</Text>
+      {order.orderItems.map((item, index) => (
+        <View key={index} style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.itemImage} />
+          <View style={styles.itemDetails}>
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemDescription}>{item.description}</Text>
+            <Text style={styles.itemPrice}>Preco: {item.price}MT</Text>
+            <Text style={styles.itemStock}>disponivel: {item.countInStock}</Text>
+            <Text style={styles.itemRating}>pontuacao: {item.rating}</Text>
+          </View>
+        </View>
+      ))}
+    <View style={{marginBottom: 210}} />
     </ScrollView>
   );
 };
@@ -41,28 +44,77 @@ const OrderDetailsScreen = (item) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
+    padding: 16,
+    backgroundColor: 'white',
+    top:20
   },
-  header: {
+  heading: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 16,
+    top:10
   },
-  section: {
-    marginBottom: 20,
-  },
-  sectionHeader: {
-    fontSize: 18,
+  subheading: {
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginVertical: 12,
   },
   label: {
     fontSize: 16,
-    marginBottom: 5,
+    fontWeight: 'bold',
+    marginVertical: 4,
   },
   value: {
     fontWeight: 'bold',
+    
+
+  },
+  price: {
+    fontWeight: 'bold',
+    
+
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    padding: 8,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  itemDetails: {
+    marginLeft: 16,
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  itemName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  itemDescription: {
+    fontSize: 14,
+    color: '#666',
+  },
+  itemPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  itemStock: {
+    fontSize: 14,
+    color: '#333',
+  },
+  itemRating: {
+    fontSize: 14,
+    color: '#333',
   },
 });
 
