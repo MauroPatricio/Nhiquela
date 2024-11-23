04bf0003abdb22996295958f558ad8f4611ce21e
Gladistone <74876119+gnhabanga@users.noreply.github.com>
2024-11-23 23:27:59 +0200

    A resolver bug, mudando o controler notification

--- backend/controllers/notificationControllerNhabanga.js
+++ backend/controllers/notificationControllerNhabanga.js
@@ -99,6 +99,7 @@ async function sendPushNotification(pushToken, message, messageID) {
   }];
 
   try {
+    console.log('seller Pushtoken: ', pushToken)
     if(pushToken.length == 0){
       console.error('Push token inválido ou nulo');
     }else{
