081eb2125e5f9085c95d13228e42eaf704a5f01a
Mauro Patricio <mpatricio@cedsif.gov.mz>
2026-07-20 17:17:57 +0200

    Actualizacao

--- backend/checkDB.js
+++ backend/checkDB.js
@@ -3,7 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const user = await mongoose.connection.db.collection('users').findOne({_id: new mongoose.Types.ObjectId('6a5e01ead01ee0c3a00faafe')}); 
-  console.log('Driver user ID:', user ? user.name : 'Not found', 'deviceToken:', user?.deviceToken); 
+  const db = mongoose.connection.db;
+  const driver = await db.collection('users').findOne({ _id: new mongoose.Types.ObjectId('6a5e01ead01ee0c3a00faafe') });
+  console.log('Driver Info:');
+  console.log('Name:', driver.name);
+  console.log('Status:', driver.status);
+  console.log('Availability:', driver.availability);
+  console.log('DeviceToken:', driver.deviceToken);
+  console.log('TransportType:', driver.deliveryman?.transport_type);
+  
+  const token = await db.collection('notificationtokens').findOne({ user: new mongoose.Types.ObjectId('6a5e01ead01ee0c3a00faafe') });
+  console.log('NotificationToken:', token ? token.deviceToken : null);
+  
+  const wallet = await db.collection('wallets').findOne({ user: new mongoose.Types.ObjectId('6a5e01ead01ee0c3a00faafe') });
+  console.log('Wallet balance:', wallet ? wallet.balance : 'No wallet');
+  
   process.exit(0);
 });
